4bcdfa97925ae40ac3b1b4075a751ec121adba1f
Mardin <mardineliassi57@gmail.com>
2022-04-20 18:35:52 +0200

    added controller and view

---
+++ src/main/java/com/example/carrentalweb/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.carrentalweb.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+
+    public String getIndex(){
+        return "home/index";
+    }
+}

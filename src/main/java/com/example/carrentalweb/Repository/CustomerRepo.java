4426b75c28938f9a3a93b8de6b1fd0fdce67875c
Mardin <mardineliassi57@gmail.com>
2022-04-21 14:23:30 +0200

    customer repository finished

--- src/main/java/com/example/carrentalweb/Repository/CustomerRepo.java
+++ src/main/java/com/example/carrentalweb/Repository/CustomerRepo.java
@@ -15,14 +15,37 @@ public class CustomerRepo {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public List<Customer> fetchAll(){
-
-        String sql = "SELECT driver_license_number, driver_since_number, first_name, last_name, phone_number, mobile_number, email, zip_table.zip_code, city FROM customer_table " +
-                "INNER JOIN zip_table ON customer_table.zip_code = zip_table.zip_code";
-        RowMapper<Customer> rowMapper = new BeanPropertyRowMapper<>(Customer.class);
-        return jdbcTemplate.query(sql,rowMapper);
-
+    public void addCustomer(Customer customer){
+        String sql = "INSERT INTO customer_table(driver_license_number, driver_since_number, first_name, last_name, phone_number, mobile_number, email, zip_table.zip_code, city) VALUES(?,?,?,?,?,?,?,?,?)";
+        jdbcTemplate.update(sql,customer.getDriver_license_number(), customer.getDriver_since_number(),
+                customer.getFirst_name(), customer.getFirst_name(), customer.getPhone_number(), customer.getMobile_number(),
+                customer.getEmail(), customer.getZip_code(), customer.getCity());
     }
 
+    public List<Customer> fetchAll() {
+        String sql = "SELECT driver_license_number, driver_since_number, first_name, last_name, phone_number, mobile_number, email, zip_table.zip_code, city FROM customer_table " + "INNER JOIN zip_table ON customer_table.zip_code = zip_table.zip_code";
+        RowMapper<Customer> rowMapper = new BeanPropertyRowMapper<>(Customer.class);
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public Customer getCustomerByLicenceNumber(String licenceNumber) {
+        String sql = "SELECT * FROM customer_table WHERE driver_licence_number = ?";
+        RowMapper<Customer> rowMapper = new BeanPropertyRowMapper<>(Customer.class);
+        Customer customer = jdbcTemplate.queryForObject(sql, rowMapper, licenceNumber);
+        return customer;
+    }
+
+    public void updateCustomer(Customer customer) {
+        String sql = "UPDATE customer_table SET driver_licence_number = ?, driver_since_number = ?, " + "first_name = ?, " +
+                "last_name = ?, phone_number = ?, mobile_number = ?, " + "email = ?, zip_code = ? city = ? WHERE id = ? ";
+        jdbcTemplate.update(sql, customer.getDriver_license_number(), customer.getDriver_since_number(),
+                customer.getFirst_name(), customer.getFirst_name(), customer.getPhone_number(), customer.getMobile_number(),
+                customer.getEmail(), customer.getZip_code(), customer.getCity());
+    }
+
+    public void deleteCustomer(String licenceNumber) {
+        String sql = "DELETE FROM customer_table WHERE driver_licence_number =?";
+        jdbcTemplate.update(sql, licenceNumber);
+    }
 
 }

c5f5b9672e6e7e59be6398133807a66f187acf67
moha <moha729g@stud.kea.dk>
2022-04-22 16:04:46 +0200

    AdminAdmin2

--- src/main/java/com/example/carrentalweb/Controller/AdminController.java
+++ src/main/java/com/example/carrentalweb/Controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.example.carrentalweb.Controller;
 
 import com.example.carrentalweb.Model.FamilyCar;
+import com.example.carrentalweb.Service.CarService;
+import com.example.carrentalweb.Service.FamilyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,30 +12,42 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class AdminController {
 
+    @Autowired
+    CarService carService;
+    @Autowired
+    FamilyService familyService;
+
+
     @GetMapping("/admin")
     public String adminMain(){
         return "admin/adminMain";
-    }
+    }//Admin Main menu
 
-    @GetMapping("/admin/view")
+    @GetMapping("/admin/carsMenu")
+    public String carsMenu(){
+        return "/admin/carsMenu";
+    }//Cars menu
+
+    @GetMapping("/admin/viewCars")
     public String viewCars(){
         return "/admin/carsMenu";
-    }
+    }//view cars only view
 
     @GetMapping("/admin/newCar")
     public String newCar (){
         return "/admin/newCar";
-    }
+    }//Choose type
 
     @GetMapping("/admin/newFamily")
     public String newFamily(){
         return "/admin/newFamily";
-    }
+    } //new family
 
     @PostMapping("/admin/newFamily")
         public String newFamily(@ModelAttribute FamilyCar familyCar){
-            return "redirect:/admin";
-
-    }
+            carService.addnew(familyCar);
+            familyService.addNew(familyCar);
+            return "redirect:/admin/carsMenu";
+    }// new family
 
 }
